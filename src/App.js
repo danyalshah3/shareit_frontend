fb61d691116c65f3dfb68fc2377d443c0fb6daee
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2022-03-06 09:24:06 -0500

    added signup form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react'
-import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom'
-import Users from './user/pages/Users.js'
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
+
+import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
-import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
+import Auth from './user/pages/Auth';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
-  return <Router>
-     <MainNavigation />
-     <main>
-    <Switch>
-   <Route path="/" exact> <Users/> </Route>
-   <Route path="/:userId/places"><UserPlaces/></Route>
-   <Route path='/places/new'> <NewPlace/> </Route>
-   <Route path="/places/:placeId"><UpdatePlace/></Route>
-   <Redirect to="/" />
-   </Switch>
-   </main>
-   </Router>
-}
+  return (
+    <Router>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Route path="/places/:placeId">
+            <UpdatePlace />
+          </Route>
+          <Route path="/auth">
+            <Auth />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </Router>
+  );
+};
 
 export default App;
